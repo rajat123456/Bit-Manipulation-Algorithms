2066d4fc2969bd02f2e2011a3ebe98de6cc51fc0
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-26 17:25:51 +0530

    Update Set the K-th bit of a given number.java

--- Set the K-th bit of a given number.java
+++ Set the K-th bit of a given number.java
@@ -5,14 +5,19 @@ public class Main
 	public static void main(String[] args) {
 		
 		int n=10;
-		int k=3; //assuming k is of 0 indexing from right to left
+		int k=2; //assuming k is of 0 indexing from right to left
+		
+		/*
+		  If k is 0 indexed from right to left than, we need to left shift 1 by "k" times.
+		  If k is 1 indexed from right to left than, we need to left shift 1 by "k-1" times.
+		*/
 		
 		set(n,k);
 	}
 	
 	static void set(int n, int k)
 	{
-	    System.out.println(n | 1<< k-1 ); 
+	    System.out.println(n | 1<< k ); 
 	}
 	
 }
