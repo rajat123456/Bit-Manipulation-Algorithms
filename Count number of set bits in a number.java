dafaa35df473ffda94573ed4969f38eae66c5dbf
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-26 18:32:06 +0530

    Update Count number of set bits in a number.java

--- Count number of set bits in a number.java
+++ Count number of set bits in a number.java
@@ -1,7 +1,7 @@
 // Count number of set bits in a number
 
-// Logic -> 1) %2==1 means odd and hence contain  1 in LSB 
-//          2) /2 for shifiting bits to right (Right shift by 1)
+/* Logic -> 1) %2==1 means odd and hence contain  1 in LSB 
+            2) /2 for shifiting bits to right (Right shift by 1) */
 
 public class Main
 {
