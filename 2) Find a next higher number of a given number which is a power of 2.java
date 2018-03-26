ba4c21ad8ab904d94fbb66db1211994f71c5e21e
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-26 22:20:56 +0530

    Update 2) Find a next higher number of a given number which is a power of 2.java

--- 2) Find a next higher number of a given number which is a power of 2.java
+++ 2) Find a next higher number of a given number which is a power of 2.java
@@ -1,6 +1,6 @@
 // Find a next higher number of a given number which is a power of 2 (Approach 2)
 
-// Logic -> Right shift the given number until we reach to 0, and then do Left shift that times which we have done right shifts.
+// Logic -> Right shift the given number until we reach to 0, and then do Left shift (of 1) that times which we have done right shifts
 
 public class Main
 {
@@ -8,6 +8,8 @@ public class Main
     {
         
         int number=32;
+        
+        //count is for counting how many times we have done right shifts
         int count=0;
         while(number !=0)
         {
@@ -16,6 +18,8 @@ public class Main
         }
         
         number=1;
+        
+        //Performing Left Shifts, to count number of times (Times upto which we have done Right Shifts)
         while(count !=0)
         {
             number =number <<1;
