e723c473cd4aab2aabd31d38dec8ea35474c91e7
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-31 13:25:13 +0530

    Update 1) Check whether the given number is power of 2.java

--- 1) Check whether the given number is power of 2.java
+++ 1) Check whether the given number is power of 2.java
@@ -1,6 +1,6 @@
 // Check whether the given number is power of 2 (Approach 1-> Using concept of counting Set Bits)
 
-/* Logic -> If we found only "one" 1 then it is a power of 2, since a number which is power of 2 has only 1 set bit. */
+/* Logic -> If we found only "one" 1 then it is a power of 2, since a number which is power of 2 has only 1 set bit. */ 
 
 public class Main
 {
